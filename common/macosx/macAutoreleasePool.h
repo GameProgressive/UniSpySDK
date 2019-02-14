dc7a1daa9180a48b21f57ae5f94a7c42096f6c24
Arves100 <arves100@gmail.com>
2019-02-14 12:59:37 +0100

    Added MacOSX AutoreleasePool file

---
+++ common/macosx/macAutoreleasePool.h
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:	gsPlatform.h
+// SDK:		GameSpy Common
+//
+// Copyright (c) IGN Entertainment, Inc.  All rights reserved.  
+// This software is made available only pursuant to certain license terms offered
+// by IGN or its subsidiary GameSpy Industries, Inc.  Unlicensed use or use in a 
+// manner not expressly authorized by IGN or GameSpy is prohibited.
+
+#ifndef _MACAUTORELEASEPOOL_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct macAutoreleasePool* macAutoreleasePoolRef;
+
+macAutoreleasePoolRef macAutoreleasePoolCreate();
+void macAutoreleasePoolRelease(macAutoreleasePoolRef pool);
+int GetPlatformPath(char **filepath);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ common/macosx/macAutoreleasePool.m
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:	gsPlatform.h
+// SDK:		GameSpy Common
+//
+// Copyright (c) IGN Entertainment, Inc.  All rights reserved.  
+// This software is made available only pursuant to certain license terms offered
+// by IGN or its subsidiary GameSpy Industries, Inc.  Unlicensed use or use in a 
+// manner not expressly authorized by IGN or GameSpy is prohibited.
+
+#import "macAutoreleasePool.h"
+#import <Foundation/Foundation.h>
+
+macAutoreleasePoolRef macAutoreleasePoolCreate()
+{
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+	return (macAutoreleasePoolRef)pool;
+}
+
+void macAutoreleasePoolRelease(macAutoreleasePoolRef pool)
+{
+	if (pool != NULL)
+		[(NSAutoreleasePool*)pool release];
+}
+
+int GetPlatformPath(char **filepath)
+{
+	static char* documentsPathCString = NULL;
+	static size_t documentsPathCStringLen = 0;
+	
+	if (documentsPathCString == NULL)
+	{
+		macAutoreleasePoolRef pool = macAutoreleasePoolCreate();	// CF types need this for automatic cleanup.
+		CFArrayRef paths = (CFArrayRef)NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+		CFStringRef documentsPathString = (CFStringRef)CFArrayGetValueAtIndex(paths, 0);
+		CFIndex bufferSize = CFStringGetMaximumSizeOfFileSystemRepresentation(documentsPathString);
+		char* buf = (char*)malloc(bufferSize);
+		CFStringGetFileSystemRepresentation(documentsPathString, buf, bufferSize);
+		macAutoreleasePoolRelease(pool);
+		documentsPathCString = realloc(buf, strlen(buf) + 1);
+		
+		if (documentsPathCString != buf)
+			free(buf);
+		
+		documentsPathCStringLen = strlen(buf);
+	}
+	
+	*filepath = documentsPathCString;
+	
+	return documentsPathCStringLen;
+}
